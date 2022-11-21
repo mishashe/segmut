ce551e8a80e312c6b8a18fb3d7ea4abdab0ae6ed
mishashe <mishashe@gmail.com>
2022-11-21 15:09:28 +0300

    Initial commit

---
+++ R/segment.R
@@ -0,0 +1,35 @@
+#' calculates geometrically averaged p-value
+#'
+#' @description calculates geometrically averaged p-value given breakpoints.
+#'
+#' @param par vector of breakpoints
+#' @param muts vector of mutations locations
+#' @param L length of locus
+#' @param Kmin minimal length of a segment
+#'
+#' @return p-value of all breakpoints after harmonic mean
+#' @export
+#'
+#' @examples
+#' pVal(par=c(3000,5000),
+#' muts =sort(c(sample(1:3000,400),sample(3001:8000,250),sample(8001:10000,400))),
+#' L=10000,Kmin=0)
+pVal <- function(par,muts,L=max(muts)-min(muts)+1,Kmin=0)
+{
+  xB <- c(0,sort(par),L)
+  if (any(diff(xB)<Kmin)) return(0)
+  pVal <- 0
+  n1 <- sum(muts>=xB[1] & muts<=xB[2])
+  K1 <- xB[2]-xB[1]
+  for (i in 2:(length(xB)-1))
+  {
+    n2 <- sum(muts>=xB[i] & muts<=xB[i+1])
+    K2 <- xB[i+1]-xB[i]
+    if (n1<10 & n2<10) return(0)
+    tau <- (n1+n2)/(K1+K2)
+    pVal <- pVal + stats::pnorm(abs(n1/K1-n2/K2), mean = 0, sd = sqrt(tau/K1+tau/K2), lower.tail=FALSE, log.p = TRUE)
+    n1 <- n2
+    K1 <- K2
+  }
+  return(pVal/length(par))
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+
+# segmut
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of segmut is to ...
+
+## Installation
+
+You can install the development version of segmut like so:
+
+``` r
+# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(segmut)
+## basic example code
+```
+
