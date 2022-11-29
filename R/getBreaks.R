3cc8ed4bafb78648b15022693682e4b09e5c69ae
Michael Sheinman <52411283+mishashe@users.noreply.github.com>
2022-11-29 10:33:11 +0300

    rmd

---
+++ R/getBreaks.R
@@ -0,0 +1,24 @@
+#' finds optimal breaks
+#'
+#' @description fits optimal breaks given number of breaks
+#'
+#' @param muts vector of mutations locations
+#' @param L length of locus
+#' @param Kmin minimal length of a segment
+#' @param n number of breaks
+#'
+#' @return results of the DE optimization
+#' @export
+getBreaks <- function(muts,L=max(muts)-min(muts)+1,Kmin=0,n)
+{
+  res <- DEoptim(pVal, lower = rep(0,n), upper = rep(L,n),
+                 control = DEoptim.control(trace = 0),
+                 muts=muts,L=L,Kmin=Kmin)
+  return(res)
+}
+
+
+
+
+
+
--- R/pVal.R
+++ R/pVal.R
@@ -32,28 +32,3 @@ pVal <- function(par,muts,L=max(muts)-min(muts)+1,Kmin=0)
   return(pVal/length(par))
 }
 
-
-
-#' finds optimal breaks
-#'
-#' @description fits optimal breaks given number of breaks
-#'
-#' @param muts vector of mutations locations
-#' @param L length of locus
-#' @param Kmin minimal length of a segment
-#'
-#' @return results of the DE optimization
-#' @export
-getBreaks <- function(muts,L=max(muts)-min(muts)+1,Kmin=0,n)
-{
-  res <- DEoptim(pVal, lower = rep(0,n), upper = rep(L,n),
-                 control = DEoptim.control(trace = 0),
-                 muts=muts,L=L)
-  return(res)
-}
-
-
-
-
-
-
