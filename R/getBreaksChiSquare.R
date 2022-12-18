ac9ee3507da9fe6adfc192a34067595dba5fc3a2
mishashe <mishashe@gmail.com>
2022-12-18 18:34:48 +0300

    GA

--- R/getBreaksChiSquare.R
+++ R/getBreaksChiSquare.R
@@ -17,9 +17,9 @@
 getChiSquare <- function(par,muts,L=max(muts)-min(muts)+1,Kmin=0)
 {
   # par <- sort(par)
-  xB <- c(0,par,L)
-  Ks <- diff(xB)
-  if (any(Ks<Kmin)) return(10)
+  xB <- c(0,par,L+1)
+  Ks <- diff(xB) + 1
+  if (any(Ks<Kmin)) return(Inf)
   expected <- (length(muts)/L)*Ks
   # nmuts <- .Internal(tabulate(.Internal(findInterval(vec=xB,x=muts, FALSE,FALSE,FALSE)), nbins=length(xB)-1))
   nmuts <- .Call(graphics:::C_BinCount, muts, xB, TRUE, TRUE) # a bit faster than nmuts <- hist(muts,breaks=xB,plot=FALSE)$counts
@@ -49,8 +49,8 @@ getBreaksChiSquare <- function(muts,L=max(muts)-min(muts)+1,Kmin=0,n=1)
                                            parallelType='none',
                                            strategy = 1,
                                            initialpop=initialpop,
-                                           storepopfreq = 10000,
-                                           reltol=1e-6),
+                                           storepopfreq = 100000,
+                                           reltol=1e-7),
                  muts=muts,L=L,Kmin=Kmin)$optim$bestmem
   return(res)
 }
@@ -78,8 +78,9 @@ getNumberBreaksChiSquare <- function(muts,L=max(muts)-min(muts)+1,Kmin=0)
   {
     n <- n + 1
     par <- getBreaksChiSquare(muts = muts, L = L, Kmin=Kmin, n=n)
-    ChiSquare <- n*log(length(muts)) + getChiSquare(par,muts,L=L,Kmin=Kmin)
-    if(ChiSquare>ChiSquarePrev | Kmin*(n+1)>=L){return(parPrev)}
+    ChiSquare <- n*log(length(muts)) + getChiSquare(par, muts, L=L, Kmin=Kmin)
+    if(ChiSquare>=ChiSquarePrev){return(parPrev)}
+    if(Kmin*(n+1)>=L){return(par)}
     parPrev <- par
     ChiSquarePrev <- ChiSquare
   }
