ce1f838309962ec206bc93c3e03a9fbc37a17712
mishashe <mishashe@gmail.com>
2022-12-18 20:36:37 +0300

    ga

--- R/getBreaksChiSquare.R
+++ R/getBreaksChiSquare.R
@@ -14,19 +14,16 @@
 # uniformity with unequal cells
 # Tadeusz Inglot a
 # & Alicja Janic-Wróblewska
-getChiSquare <- function(par,muts,L=max(muts)-min(muts)+1,Kmin=0)
+getChiSquare <- function(par,muts,L=max(muts)-min(muts)+1,Kmin=0,parPrev=c())
 {
-  # par <- sort(par)
-  xB <- c(0,par,L+1)
+  xB <- c(0,sort(c(par,parPrev)),L+1)
   Ks <- diff(xB) + 1
-  if (any(Ks<Kmin)) return(Inf)
+  if (any(Ks<Kmin)) return(-Inf)
   expected <- (length(muts)/L)*Ks
-  # nmuts <- .Internal(tabulate(.Internal(findInterval(vec=xB,x=muts, FALSE,FALSE,FALSE)), nbins=length(xB)-1))
   nmuts <- .Call(graphics:::C_BinCount, muts, xB, TRUE, TRUE) # a bit faster than nmuts <- hist(muts,breaks=xB,plot=FALSE)$counts
   chiSquare <- sum((nmuts - expected)^2/expected)
-  return(-chiSquare)
+  return(chiSquare)
 }
-
 
 #' finds optimal breaks ChiSquare
 #'
@@ -40,19 +37,23 @@ getChiSquare <- function(par,muts,L=max(muts)-min(muts)+1,Kmin=0)
 #' @return results of the DE optimization
 #' @export
 #'
-getBreaksChiSquare <- function(muts,L=max(muts)-min(muts)+1,Kmin=0,n=1)
+getBreaksChiSquare <- function(muts,L=max(muts)-min(muts)+1,Kmin=0,n=1,parPrev=c())
 {
-  initialpop <- Kmin + 1 + matrix(runif(20*1*n)*(L-2*Kmin-3),ncol=n)
-  if (n>1) initialpop=t(apply(initialpop,1,sort))
-  res <- DEoptim(getChiSquare, lower = rep(Kmin,n), upper = rep(L-Kmin,n),
-                 control = DEoptim.control(trace = FALSE,
-                                           parallelType='none',
-                                           strategy = 1,
-                                           initialpop=initialpop,
-                                           storepopfreq = 100000,
-                                           reltol=1e-7),
-                 muts=muts,L=L,Kmin=Kmin)$optim$bestmem
-  return(res)
+  res <- ga(type = "real-valued", fitness = getChiSquare,optim=FALSE,keepBest=FALSE,
+            lower = rep(Kmin,1),
+            upper = rep(L-Kmin,1),
+            parallel=FALSE,
+            monitor = FALSE,
+            muts=muts,L=L,Kmin=Kmin,parPrev=parPrev)@solution
+  res <- sort(c(parPrev,res))
+  suggestions <- matrix(res,nrow=1)
+  res <- ga(type = "real-valued", fitness = getChiSquare,optim=FALSE,keepBest=FALSE,
+            lower = res-diff(c(0,res))/2,
+            upper = res+diff(c(res,L))/2,
+            parallel=FALSE,
+            suggestions=suggestions,monitor = FALSE,
+           muts=muts,L=L,Kmin=Kmin,parPrev=c())@solution
+  return(sort(c(res)))
 }
 
 
@@ -68,23 +69,25 @@ getBreaksChiSquare <- function(muts,L=max(muts)-min(muts)+1,Kmin=0,n=1)
 #' @return breaks locations for optimal number of breaks
 #' @export
 #'
-getNumberBreaksChiSquare <- function(muts,L=max(muts)-min(muts)+1,Kmin=0)
+getNumberBreaksChiSquare <- function(muts,L=max(muts)-min(muts)+1,Kmin=0, nmax=0)
 {
   n <- 0
   parPrev <- c()
-  if (length(muts)<2 | (Kmin*2+2)>=L) return(parPrev)
-  ChiSquarePrev <- 0*log(length(muts)) + getChiSquare(parPrev,muts,L=L,Kmin=Kmin)
+  ChiSquareVector <- c(-getChiSquare(parPrev,muts,L=L,Kmin=Kmin))
+  parBest <- parPrev
+  if (length(muts)<2 | (Kmin*2+2)>=L) return((parBest))
+  ChiSquarePrev <- 0*log(length(muts)) - ChiSquareVector[1]
   repeat
   {
     n <- n + 1
-    par <- getBreaksChiSquare(muts = muts, L = L, Kmin=Kmin, n=n)
-    ChiSquare <- n*log(length(muts)) + getChiSquare(par, muts, L=L, Kmin=Kmin)
-    if(ChiSquare>=ChiSquarePrev){return(parPrev)}
-    if(Kmin*(n+1)>=L){return(par)}
+    par <- getBreaksChiSquare(muts = muts, L = L, Kmin=Kmin, n=n, parPrev)
+    ChiSquare <- n*log(length(muts)) - getChiSquare(par, muts, L=L, Kmin=Kmin)
+    ChiSquareVector <- c(ChiSquareVector,ChiSquare)
+    if(ChiSquare<ChiSquarePrev) parBest <- par
+    if(ChiSquare>=ChiSquarePrev & ChiSquare>=ChiSquarePrev){return((parBest))}
+    if(Kmin*(n+1)>=L){return((parBest))}
     parPrev <- par
     ChiSquarePrev <- ChiSquare
   }
 }
-
-
 
